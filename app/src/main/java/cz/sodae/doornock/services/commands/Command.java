c0b5fd890b16c4d7f5d77f1c6bbb4a786884c953
Martin Sadovy <sodae@email.cz>
2016-03-20 20:56:48 +0100

    Command: optimalization algorithm

--- app/src/main/java/cz/sodae/doornock/services/commands/Command.java
+++ app/src/main/java/cz/sodae/doornock/services/commands/Command.java
@@ -25,12 +25,6 @@ public class Command
 
 
     public Command(byte[] apdu) throws ApduInvalidLengthException {
-        this(
-                apdu[0], apdu[1], apdu[2], apdu[3],
-                (int) apdu[apdu.length - 1],
-                Arrays.copyOfRange(apdu, 5, 5 + apdu[4])
-        );
-
         this.defClass = apdu[0];
         this.defInstruction = apdu[1];
         this.defParameter = apdu[2];
@@ -42,55 +36,45 @@ public class Command
             return;
         }
 
-        if (apdu[4] == 0x00) { // 5th byte is zero - APDU extended
-            if (apdu.length == 4 + 3) { // header + 3 bytes Le
-                if (apdu[5] == 0x00 && apdu[6] == 0x00) {
-                    this.defMaximumLengthResponse = 65536;
-                } else {
-                    this.defMaximumLengthResponse = twoByteToInt(apdu[5], apdu[6]);
-                }
-            } else if (apdu.length == 4 + 1) { // header + 1 bytes Le
-                if (apdu[5] == 0x00) {
-                    this.defMaximumLengthResponse = 256;
-                } else {
-                    this.defMaximumLengthResponse = apdu[5];
-                }
+        // Algorithm to parse APDU Extended
 
-            } else { // header + Lc + Nc + Le
-                int contentSize = twoByteToInt(apdu[5], apdu[6]);
-                this.defData = Arrays.copyOfRange(apdu, 7, 7 + contentSize);
+        int LePosition = 0;
+        int LcPosition = 0;
+        int LcSize = 0;
+        int dataSize = 0;
+        int dataPosition = 0;
 
-                int LePosition = 4 + 3 + contentSize - 1;
-                if (apdu.length == (LePosition + 1)) { // header + Lc + content + (1 byte) Le
-                    if (apdu[LePosition] == 0x00) {
-                        this.defMaximumLengthResponse = 256;
-                    } else {
-                        this.defMaximumLengthResponse = apdu[LePosition];
-                    }
-                } else if (apdu.length == (LePosition + 1 + 2)) { // 2 byte Le
-                    if (apdu[LePosition] == 0x00 && apdu[LePosition+1] == 0x00) {
-                        this.defMaximumLengthResponse = 65536;
-                    } else {
-                        this.defMaximumLengthResponse = twoByteToInt(apdu[LePosition], apdu[LePosition+1]);
-                    }
-                }
+        if (apdu.length == 4 + 1) { // when there is only Le
+            LePosition = 4;
+        } else if (apdu[4] == 0x00) { // extended APDU, Lc/Le is/are 3 bytes
+            if (apdu.length == 4 + 3) { // when only Le
+                LePosition = 5;
+            } else { // always when Lc = 3 bytes
+                LcPosition = 5;
+                LcSize = 2;
+                dataSize = twoByteToInt(apdu[LcPosition], apdu[LcPosition + 1]);
+                dataPosition = LcPosition + LcSize;
+                LePosition = dataPosition + dataSize;
             }
-        } else {
-            this.defData = Arrays.copyOfRange(apdu, 4, 4 + apdu[4]);
-            int LePosition = 4 + 1 + apdu[4] - 1;
+        } else { // when Lc is 1 byte
+            dataPosition = 5;
+            dataSize = apdu[4];
+            LePosition = dataPosition + dataSize;
+        }
+
+        if (LePosition != 0) {
+            int max = 0;
             if (apdu.length == LePosition + 1 + 2) {
-                if (apdu[LePosition] == 0x00 && apdu[LePosition+1] == 0x00) {
-                    this.defMaximumLengthResponse = 65536;
-                } else {
-                    this.defMaximumLengthResponse = twoByteToInt(apdu[LePosition], apdu[LePosition+1]);
-                }
+                max = twoByteToInt(apdu[LePosition], apdu[LePosition + 1]);
+                this.defMaximumLengthResponse = max == 0 ? 65536 : max;
             } else if (apdu.length == LePosition + 1 + 1) {
-                if (apdu[LePosition] == 0x00) {
-                    this.defMaximumLengthResponse = 256;
-                } else {
-                    this.defMaximumLengthResponse = apdu[LePosition];
-                }
+                max = apdu[LePosition];
+                this.defMaximumLengthResponse = max == 0 ? 256 : max;
             }
+        }
+
+        if (dataPosition != 0) {
+            this.defData = Arrays.copyOfRange(apdu, dataPosition, dataPosition + dataSize);
         }
 
     }
